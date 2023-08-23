e24abc5ad7c99df877ab85d3ac83efef9ed052ec
Jonas Spenger <6832643+jonasspenger@users.noreply.github.com>
2023-08-23 15:51:42 +0200

    Lowered the rate limit by default with logging

--- code/src/main/scala/portals/vldb2023demo/shoppingcart/ShoppingCartConfig.scala
+++ code/src/main/scala/portals/vldb2023demo/shoppingcart/ShoppingCartConfig.scala
@@ -3,9 +3,9 @@ package portals.vldb2023demo.shoppingcart
 object ShoppingCartConfig:
   inline val N_EVENTS = 1024 * 1024 * 1024
   inline val N_ATOM_SIZE = 5
-  inline val RATE_LIMIT = 64
-  // inline val RATE_LIMIT = 4
+  // inline val RATE_LIMIT = 64
+  inline val RATE_LIMIT = 4
   inline val N_USERS = 128
   inline val N_ITEMS = 1024
-  inline val LOGGING = false
-  // inline val LOGGING = true
+  // inline val LOGGING = false
+  inline val LOGGING = true
